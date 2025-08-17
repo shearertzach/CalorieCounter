1ea4627fe78e3e034b783b41c02c25f5f2e3326a
Zach Shearer <shearertzach@gmail.com>
2025-08-16 22:17:19 -0500

    fix auth issue again

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -26,7 +26,7 @@ export default function SignIn() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: process.env.NEXT_PUBLIC_SITE_URL || `${window.location.origin}`,
+          redirectTo: `${window.location.origin}`,
           queryParams: {
             access_type: 'offline',
             prompt: 'consent',
